ca0f748771e80b41d97d88f88873d6de77b6c73f
yasirarafatalif <yasirarafatalif1@gmail.com>
2026-03-18 15:18:00 +0600

    products-collection page api add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(cors());
 
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.zgnatwl.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,84 @@ async function run() {
     app.get("/products", async (req, res) => {
       const cursor = await productsCollections.find({}).toArray();
       res.send(cursor);
+    });
+
+    // collection page api
+    app.get("/products-collections", async (req, res) => {
+      const { category, size } = req.query;
+       if (size) {
+        const allData = await productsCollections.find({"inventory.size": size,}).toArray();
+        return res.send(allData);
+      }
+      if (!category || category === "all") {
+        const allData = await productsCollections.find({}).toArray();
+        return res.send(allData);
+      }
+      if (size) {
+        const allData = await productsCollections.find({"inventory.size": size,}).toArray();
+        return res.send(allData);
+      }
+      
+      if (category === "MEN" && !size) {
+        const menData = await productsCollections
+          .find({ gender: "MEN" })
+          .toArray();
+        return res.send(menData);
+      }
+      if (category === "MEN" && size) {
+        const menData = await productsCollections
+          .find({
+            gender: "MEN",
+            "inventory.size": size,
+          })
+          .toArray();
+
+        return res.send(menData);
+      }
+      if (category === "WOMEN" && !size) {
+        const menData = await productsCollections
+          .find({ gender: "WOMEN" })
+          .toArray();
+
+        return res.send(menData);
+      }
+
+       if (category === "WOMEN" && size) {
+        const menData = await productsCollections
+          .find({ gender: "WOMEN" ,"inventory.size": size, })
+          .toArray();
+
+        return res.send(menData);
+      }
+      if (category && size) {
+        const menData = await productsCollections
+          .find({ category,"inventory.size": size, })
+          .toArray();
+
+        return res.send(menData);
+      }
+
+      const cursor = await productsCollections.find({ category }).toArray();
+      res.send(cursor);
+    });
+
+    // single data api
+    app.get("/products/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = { _id: new ObjectId(id) };
+
+        const product = await productsCollections.findOne(query);
+
+        if (!product) {
+          return res.status(404).send({ message: "Product not found" });
+        }
+
+        res.send(product);
+      } catch (error) {
+        res.status(500).send({ message: "Server error", error });
+      }
     });
 
     // app.get("/home-collections", async (req, res) => {
@@ -69,7 +147,13 @@ async function run() {
 
       if (!gender || gender === "all") {
         const allData = await productsCollections.find({}).toArray();
-        return res.send(allData); 
+        return res.send(allData);
+      }
+      if (gender === "T-SHIRTS") {
+        const shirtsData = await productsCollections
+          .find({ category: "T-SHIRTS" })
+          .toArray();
+        return res.send(shirtsData);
       }
 
       const result = await productsCollections.find({ gender }).toArray();
